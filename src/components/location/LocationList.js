de687fca05931779dee09ca5cf2807711ea839bf
Brittany Ramos-Janeway <bramosjaneway@gmail.com>
2019-01-17 12:40:21 -0600

    completed pratice with location

---
+++ src/components/location/LocationList.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+
+export default class LocationList  extends Component {
+    render() {
+        return (
+            <article>
+                <h1>Student Kennels</h1>
+                <section>
+                    <h4>Nashville North Location</h4>
+                    <h5>500 Tail Trail</h5>
+                </section>
+                <section>
+                    <h4>Nashville South Location</h4>
+                    <h5>33 Paw Place</h5>
+                </section>
+            </article>
+        );
+    }
+}
